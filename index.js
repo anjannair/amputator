eff26b821f9b719e88126403ed362c6aa6a27773
Anjan Nair <22571613+anjannair@users.noreply.github.com>
2020-12-04 21:14:40 +0530

    Added checking for non links

--- index.js
+++ index.js
@@ -3,38 +3,52 @@ const cheerio = require('cheerio');
 const { JSDOM } = require("jsdom");
 
 module.exports = class Amp {
+
+  isValidURL(string) {
+    //regex for link
+    const res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (res !== null);
+  }
+
   async canon(link) {
+    //The final output
+    let final;
 
-  //The final output
-  let final;
+    if (this.isValidURL(link) == false) {
+      final = {
+        "success": false,
+        "reason": "Link is not valid",
+      }
+      return final;
+    }
 
-  //Checks if the canonical links is found
-  let found = false;
+    //Checks if the canonical links is found
+    let found = false;
 
-  //Fetching the HTML using axios
-  const { data } = await axios.get(link);
+    //Fetching the HTML using axios
+    const { data } = await axios.get(link);
 
-  //Using cheerio to load the HTML fetched
-  const $ = await cheerio.load(data);
+    //Using cheerio to load the HTML fetched
+    const $ = await cheerio.load(data);
 
-  //Fetching the title of the site
-  const title = await $('title')[0].children[0].data;
+    //Fetching the title of the site
+    const title = await $('title')[0].children[0].data;
 
-  //Gets the canonical (non-amp) link from the HTML
-  const canonical = await $('link[rel = "canonical"]')[0].attribs.href;
+    //Gets the canonical (non-amp) link from the HTML
+    const canonical = await $('link[rel = "canonical"]')[0].attribs.href;
 
-  //Checks if canonical is present
-  if (canonical) found = true;
+    //Checks if canonical is present
+    if (canonical) found = true;
 
-  //If no canonical then return not found
-  if (!canonical) canonical = "Not Found";
+    //If no canonical then return not found
+    if (!canonical) canonical = "Not Found";
 
-  //Pushes the final API
-  final = await {
-    "success": found,
-    "title": title,
-    "canonical": canonical,
-  };
-  return final;
-}
+    //Pushes the final API
+    final = await {
+      "success": found,
+      "title": title,
+      "canonical": canonical,
+    };
+    return final;
+  }
 }
